b98b4d5d3614c8d046328953241b283b647b59bc
oluwadamilareolusakin <oluwadamilareo@fixus.com.ng>
2020-05-28 20:30:59 +0100

    Setup components

--- src/components/todays-events/TodaysEvents.component.jsx
+++ src/components/todays-events/TodaysEvents.component.jsx
@@ -1,24 +1,45 @@
-import React, { useState } from 'react';
-import Input from '../input/Input.component';
+import React, { useEffect } from 'react';
+import PropTypes, { objectOf } from 'prop-types';
+import EventCard from '../event-card/EventCard.component';
+import Button from '../button/Button.component';
 
-const TodaysEvents = () => {
-    const [events, setEvents] = useState({events: []})
-
+const TodaysEvents = ( { fetchEvents, events, openEventModal } ) => {
     useEffect(() => {
         const fetchData = async() => {
-            const result = await Axios.get("")
+            fetchEvents({today: true});
         }
-
         fetchData();
     }, [])
+
+    const removeFromList = id => {
+        removeEvent(id);
+    }
+
+    const removeEvent = (id) => {
+        events = events.filter(event => event.id != id);
+    }
 
     return (
         <div className="todays-events">
             {
-               events && events.map((event, index) => <EventCard key={index} event={event}/>)
+               events.length ? events.map((event, index) => <EventCard key={index} event={event} removeFromList={removeFromList} />) :
+               <div className="empty-event-list">
+                   <h3>No events here yet! Make some?</h3>
+                   <Button buttonAction={openEventModal}>New Event</Button>
+               </div>
             }
         </div>
     )
 };
+
+TodaysEvents.defaultProps = {
+    fetchEvents: () => {},
+    events: [],
+};
+
+TodaysEvents.propTypes = {
+    fetchEvents: PropTypes.func,
+    events: PropTypes.arrayOf(objectOf(PropTypes.string))
+}
 
 export default TodaysEvents;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './components/App.component';
+import './index.scss';
+
 const Index = () => {
     return (
         <App />
