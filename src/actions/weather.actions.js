6d670ffefa26726f6d786d246fcc48daef97a4bb
oluwadamilareolusakin <oluwadamilareo@fixus.com.ng>
2020-05-30 10:37:28 +0100

    Add fresh weather API keys

--- src/actions/weather.actions.js
+++ src/actions/weather.actions.js
@@ -1,11 +1,11 @@
 import Axios from "axios"
 
 export const weatherAPI = Axios.create({
-    // baseURL:  "https://community-open-weather-map.p.rapidapi.com",
+    baseURL:  "https://community-open-weather-map.p.rapidapi.com",
     headers: {
         "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-        "x-rapidapi-key": "71c31ec718msh2de722c911d7970p158f55jsn6f54764ea00e",
-        "useQueryString": true,
+        "x-rapidapi-key": "db3236f968mshc12d9100de1580ap188207jsnde8adb187409",
+        "useQueryString": true
     }
 })
 
